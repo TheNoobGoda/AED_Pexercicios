b23fda18098fa227968b63015f73308d5472f5e8
YTheNoobGoda <davidespinhamarques@gmail.com>
2022-01-11 23:20:31 +0000

    yes

--- aed2122_p01/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/david/Desktop/FEUP/aed/aed2122_p01/lib/googletest-master
-# Build directory: C:/Users/david/Desktop/FEUP/aed/aed2122_p01/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- aed2122_p01/cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/david/Desktop/FEUP/aed/aed2122_p01/lib/googletest-master/googletest
-# Build directory: C:/Users/david/Desktop/FEUP/aed/aed2122_p01/cmake-build-debug/lib/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
